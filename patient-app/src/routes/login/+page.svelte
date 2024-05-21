1cfb9f88a0f240661159db74c7f39d10c6e2e96c
Filipe Santos <43495087+FCardeira@users.noreply.github.com>
2024-05-21 14:22:05 +0100

    Frontend app boilerplate

---
+++ patient-app/src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<Navbar fluid  >
+    <NavBrand href="/">
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <NavHamburger  />
+    <NavUl >
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+  </Navbar>
+
+  <script lang="ts">
+      import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  </script>
